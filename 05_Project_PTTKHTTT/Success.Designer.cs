b1912fdd58f8a47a79457808d15aad28185c6f18
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-06-26 18:07:55 +0700

    thêm

---
+++ 05_Project_PTTKHTTT/Success.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _05_Project_PTTKHTTT
+{
+    partial class Success
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_back = new Button();
+            lb_Confirm = new Label();
+            SuspendLayout();
+            // 
+            // btn_back
+            // 
+            btn_back.BackColor = Color.White;
+            btn_back.BackgroundImage = Properties.Resources.button_round22;
+            btn_back.FlatAppearance.BorderSize = 0;
+            btn_back.FlatStyle = FlatStyle.Flat;
+            btn_back.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_back.ForeColor = SystemColors.Window;
+            btn_back.Location = new Point(200, 145);
+            btn_back.Name = "btn_back";
+            btn_back.Size = new Size(91, 38);
+            btn_back.TabIndex = 58;
+            btn_back.Text = "Quay lại";
+            btn_back.UseVisualStyleBackColor = false;
+            btn_back.Click += btn_back_Click;
+            // 
+            // lb_Confirm
+            // 
+            lb_Confirm.AutoSize = true;
+            lb_Confirm.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lb_Confirm.Location = new Point(181, 69);
+            lb_Confirm.Name = "lb_Confirm";
+            lb_Confirm.Size = new Size(143, 31);
+            lb_Confirm.TabIndex = 56;
+            lb_Confirm.Text = "Thành công!";
+            // 
+            // Success
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImage = Properties.Resources.bg_confirm;
+            ClientSize = new Size(485, 240);
+            Controls.Add(btn_back);
+            Controls.Add(lb_Confirm);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Success";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Success";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_back;
+        private Label lb_Confirm;
+    }
+}
